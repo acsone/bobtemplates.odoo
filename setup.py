036cab9625a49f5f87887fe5dfd1b65a111a230e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-06-15 21:12:39 +0200

    fix trove classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: '
-        'GNU Affero General Public License v3 or later (AGPLv3+)'
+        'GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
