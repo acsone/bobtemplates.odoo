783f9253ad131a7fb43141049040dc06ef1d2e4a
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-06-15 22:51:17 +0200

    use to_boolean hook from mrbob package

--- bobtemplates/odoo/hooks.py
+++ bobtemplates/odoo/hooks.py
@@ -5,28 +5,6 @@
 from mrbob.bobexceptions import ValidationError
 
 
-def to_boolean(configurator, question, answer):
-    """
-    If you want to convert an answer to Python boolean, you can
-    use this function as :ref:`post-question-hook`:
-    .. code-block:: ini
-        [questions]
-        idiot.question = Are you young?
-        idiot.post_ask_question = mrbob.hooks:to_boolean
-    Following variables can be converted to a boolean:
-    **y, n, yes, no, true, false, 1, 0**
-    """
-    if isinstance(answer, bool):
-        return answer
-    value = answer.lower()
-    if value in ['y', 'yes', 'true', '1']:
-        return True
-    elif value in ['n', 'no', 'false', '0']:
-        return False
-    else:
-        raise ValidationError('Value must be a boolean (y/n)')
-
-
 def _camelcase(dotted):
     return ''.join([s.capitalize() for s in dotted.split('.')])
 
