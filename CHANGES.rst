f221e1b98509411f75de1c73ecc7a51a17b6a0f1
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-06-16 08:58:09 +0200

    model post render message; add model import in __init__.py

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,12 @@ Changes
 .. ----------
 .. -
 
+1.0.0a3 (2016-06-16)
+--------------------
+- add post render message inviting the user to add the view xml in
+  __openerp__.py data section
+- auto add model import to models/__init__.py
+
 1.0.0a2 (2016-06-15)
 --------------------
 - fix broken namespace package distribution
--- bobtemplates/odoo/hooks.py
+++ bobtemplates/odoo/hooks.py
@@ -2,20 +2,42 @@
 # Copyright © 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import os
+
 from mrbob.bobexceptions import ValidationError
+from mrbob.hooks import show_message
 
 
-def _camelcase(dotted):
+def _dotted_to_camelcased(dotted):
     return ''.join([s.capitalize() for s in dotted.split('.')])
 
 
-def _underscore(dotted):
+def _dotted_to_underscored(dotted):
     return dotted.replace('.', '_')
 
 
-def post_model_name_dotted(configurator, question, answer):
+def _underscored_to_camelwords(underscored):
+    return ' '.join([s.capitalize() for s in underscored.split('_')])
+
+
+def post_question_model_name_dotted(configurator, question, answer):
     if '.' not in answer:
         raise ValidationError('Name must contain a dot')
-    configurator.variables['model.name_underscored'] = _underscore(answer)
-    configurator.variables['model.name_camelcased'] = _camelcase(answer)
+    configurator.variables['model.name_underscored'] = \
+        _dotted_to_underscored(answer)
+    configurator.variables['model.name_camelcased'] = \
+        _dotted_to_camelcased(answer)
     return answer
+
+
+def post_render_model(configurator):
+    init_path = os.path.join(configurator.target_directory,
+                             'models', '__init__.py')
+    #if os.path.exists(init_path):
+    #    mode = 'a'
+    #else:
+    #    mode = 'w'
+    with open(init_path, 'a') as init_file:
+        init_file.write('from . import {}\n'.format(
+            configurator.variables['model.name_underscored']))
+    show_message(configurator)
