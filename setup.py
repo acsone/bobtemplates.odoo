9939a251498333d08975bc55bdeb1900ea25edf0
Adrien Peiffer <adrien.peiffer@acsone.eu>
2022-12-14 08:48:40 +0100

    Remove support of python 3.6

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
     namespace_packages=["bobtemplates"],
     include_package_data=True,
     setup_requires=["setuptools_scm"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
